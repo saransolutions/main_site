90779210eb501d1efa09d10bd258b9b8a4ce7d7e
Sundaravel Natarajan <sundaravel.natarajan@swisscom.com>
2024-05-23 08:09:18 +0200

    updated

---
+++ old/contact_section.php
@@ -0,0 +1,30 @@
+<?php
+function after_header(){
+    return '<div class="j-menu-container"></div>
+
+    <div class="b-inner-page-header f-inner-page-header b-bg-header-inner-page">
+      <div class="b-inner-page-header__content">
+        <div class="container">
+          <h1 class="f-primary-l c-default">Contact us</h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="l-main-container">
+        <div class="b-breadcrumbs f-breadcrumbs">
+            <div class="container">
+                <ul>
+                    <li><a href="#"><i class="fa fa-home"></i>Home</a></li>
+                    <li><i class="fa fa-angle-right"></i><span> Contact us v2</span></li>
+                </ul>
+            </div>
+        </div>
+      
+        ';
+    }
+
+
+?>
+
+
+		
