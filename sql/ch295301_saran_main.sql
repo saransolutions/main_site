5b0e6e435bc56e3d38fd091cae1171f5916ce04d
Sundaravel Natarajan <sundaravel.natarajan@swisscom.com>
2024-06-09 11:22:05 +0200

    updated

--- sql/ch295301_saran_main.sql
+++ sql/ch295301_saran_main.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2024 at 02:32 AM
+-- Generation Time: Jun 09, 2024 at 11:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
